56c9b8dbac29875b0841a997fb0e6f6c06cf41b9
Dino <dino2gnt@gmail.com>
2024-08-30 19:00:14 -0500

    words

--- plugin/src/main/java/org/opennms/timeseries/impl/pgtimeseries/shell/Stats.java
+++ plugin/src/main/java/org/opennms/timeseries/impl/pgtimeseries/shell/Stats.java
@@ -1,7 +1,6 @@
 
 package org.opennms.timeseries.impl.pgtimeseries.shell;
 
-
 import java.util.concurrent.TimeUnit;
 
 import org.apache.karaf.shell.api.action.Action;
@@ -12,7 +11,8 @@ import org.opennms.timeseries.impl.pgtimeseries.PGTimeseriesStorage;
 
 import com.codahale.metrics.ConsoleReporter;
 
-@Command(scope = "opennms-pgtimeseries", name = "stats", description = "Display statistics from the pg_timeseries plugin.")
+
+@Command(scope = "opennms-pgtimeseries", name = "stats", description = "Display metrics from the pg_timeseries plugin.")
 @Service
 public class Stats implements Action {
 
