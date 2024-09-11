4cdef2cbb84a8f6385cfa3bb72c8ba14efc74855
Dino <dino2gnt@gmail.com>
2024-09-11 15:32:21 -0500

    Alter queries to return NaN / NULL for time buckets which have no data

--- plugin/src/main/java/org/opennms/timeseries/impl/pgtimeseries/PGTimeseriesStorage.java
+++ plugin/src/main/java/org/opennms/timeseries/impl/pgtimeseries/PGTimeseriesStorage.java
@@ -287,24 +287,24 @@ public class PGTimeseriesStorage implements TimeSeriesStorage {
                     //this Common Table Expression computes the delta between measurements and sums over the interval period, e.g. a counter with no aggregation
                     sql = String.format("with raw_values as " +
                             "( select time AS step, value - LAG(value) OVER (ORDER BY time) " +
-                            "as deltaval FROM pgtimeseries_time_series where key=? AND time > %s AND time < %s ORDER BY step ASC ) " +
-                            "select step, (sum(deltaval) / %s) as aggregation from raw_values GROUP BY step ORDER BY step", end, start, stepInSeconds);
+                            "as deltaval FROM pgtimeseries_time_series where (key=? OR key is null) AND time > %s AND time < %s ORDER BY step ASC ) " +
+                            "select step, COALESCE((sum(deltaval) / %s),'NaN') as aggregation from raw_values GROUP BY step ORDER BY step", end, start, stepInSeconds);
                 } else {
                     //this Common Table Expression computes the delta between measurements and performs an aggregation function on the
                     // time-grouped deltas e.g. an aggregated counter
                     sql = String.format("with raw_values as " +
                             "( select time AS step, value - LAG(value) OVER (ORDER BY time) " +
-                            "as deltaval FROM date_bin_table(NULL::pgtimeseries_time_series, '%s Seconds', '[%s, %s]') where key=?) " +
-                            "select step, (%s(deltaval) / %s) as aggregation from raw_values GROUP BY step ORDER BY step",
+                            "as deltaval FROM date_bin_table(NULL::pgtimeseries_time_series, '%s Seconds', '[%s, %s]') where (key=? OR key is null)) " +
+                            "select step, COALESCE((%s(deltaval) / %s), 'NaN') as aggregation from raw_values GROUP BY step ORDER BY step",
                             stepInSeconds, start, end, toSql(request.getAggregation()), stepInSeconds);
                 }
             } else {
                 if (Aggregation.NONE == request.getAggregation()) {
-                    sql = String.format("SELECT time AS step, value as aggregation FROM pgtimeseries_time_series where key=? AND time > %s AND time < %s ORDER BY step ASC", end, start);
+                    sql = String.format("SELECT time AS step, COALESCE(value, 'NaN') as aggregation FROM pgtimeseries_time_series where (key=? OR key is null) AND time > %s AND time < %s ORDER BY step ASC", end, start);
                 } else {
-                    sql = String.format("SELECT time AS step, %s(value) as aggregation" +
+                    sql = String.format("SELECT time AS step, COALESCE(%s(value), 'NaN') as aggregation" +
                             " FROM date_bin_table(NULL::pgtimeseries_time_series, '%s Seconds', '[%s, %s]') " +
-                            "where key=? GROUP BY step", toSql(request.getAggregation()), stepInSeconds, start, end);
+                            "where (key=? OR key is null) GROUP BY step ORDER BY step", toSql(request.getAggregation()), stepInSeconds, start, end);
                 }
             }
             PreparedStatement statement = connection.prepareStatement(sql);
